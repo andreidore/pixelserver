44237aba630d805f1f727b5e2db723a39d161128
Andrei Dore <andrei.dore@gmail.com>
2018-08-21 11:23:33 +0300

    new test script

--- client/test.py
+++ client/test.py
@@ -12,7 +12,7 @@ def main():
     while True:
         
         data=[]
-        for i in range(160):
+        for i in range(558):
             
             data.append({"i":i,"r":color,"g":color+10,"b":color+10})
             
@@ -27,6 +27,33 @@ def main():
 
 
 
+def main2():
+     print("start")
+     idx=0
+     while True:
+            
+            
+            
+            data=[]
+            for i in range(558):
+                
+                if idx==i:
+                   data.append({"i":i,"r":255,"g":255,"b":0})
+                else:
+                   data.append({"i":i,"r":0,"g":0,"b":0}) 
+
+                
+            
+            #print(data)
+            r = requests.post('http://192.168.1.124:9000/api', json = data)
+            time.sleep(0.005)
+                
+            idx+=1
+
+            if idx==558:
+                idx=0
+
+                
 
 
 
@@ -34,4 +61,4 @@ def main():
 
 
 if __name__=="__main__":
-    main()
+    main2()
